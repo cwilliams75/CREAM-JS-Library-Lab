ee618d4a19e3895e4c96424435677f37f92e33fd
CWilliams75 <coreywllms9@gmail.com>
2021-01-20 10:41:38 -0500

    initial set up for Linkedlist project

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -1,5 +1,25 @@
 class LinkedList {
-
-}
-
-module.exports = LinkedList;
+    constructor(head = null) {
+      this.head = head;
+    }
+    add(element) {
+    }
+    addAt(index) {
+    }
+    remove() {
+    }
+    removeAt() {
+    }
+    contains() {
+        return true;
+    }
+    size() {
+    }
+    clear() {
+    }
+    getLast() {
+    }
+    getFirst() {
+    }
+  }
+  module.exports = LinkedList;
--- linkedList/LinkedList.test.js
+++ linkedList/LinkedList.test.js
@@ -1,5 +1,15 @@
-const { TestScheduler } = require('jest');
-const LinkedList = require('./LinkedList');
+const { TestScheduler } = require("jest");
+const LinkedList = require("./LinkedList");
 
 test("First Test", () => {
+  // Given
+ const linkedList = new LinkedList();
+
+  // When
+  linkedList.add(1);
+  const expected = true;
+  const actual = linkedList.contains(1);
+
+  // Then
+  expect(expected).toEqual(actual);
 });
