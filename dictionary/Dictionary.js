d0c39db6f1a0aa64936987a399d807674de3a93b
CWilliams75 <coreywllms9@gmail.com>
2021-01-20 22:46:50 -0500

    added Dictionary data structure

--- dictionary/Dictionary.js
+++ dictionary/Dictionary.js
@@ -1,5 +1,18 @@
 class Dictionary {
 
+  constructor() {
+    this.data = {};
+  }
+  add(key, value) {
+    this.data[key] = value;
+  }
+  get(key) {
+    return this.data[key];
+  }
 }
+const dictionary = new Dictionary();
+dictionary.add("Undefined Integers", { name: "CaptainCorey" });
+
+//const undefinedIntegers = dictionary.get("undefined integers");
 
 module.exports = Dictionary;
--- dictionary/Dictionary.test.js
+++ dictionary/Dictionary.test.js
@@ -1,5 +1,11 @@
-const { TestScheduler } = require('jest');
-const Dictionary = require('./Dictionary');
+const { TestScheduler } = require("jest");
+const Dictionary = require("./Dictionary");
 
 test("First Test", () => {
+  //GIVEN
+  const dictionary = new Dictionary();
+  //WHEN
+  dictionary.add("key", "value");
+  //THEN
+  expect(dictionary.get("key")).toEqual("value");
 });
