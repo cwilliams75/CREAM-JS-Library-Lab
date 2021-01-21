26bae34cca40c07505dcd6cb357d87629b07c75a
CWilliams75 <coreywllms9@gmail.com>
2021-01-21 12:11:31 -0500

    added more methods to the dictionary structure

--- dictionary/Dictionary.js
+++ dictionary/Dictionary.js
@@ -1,18 +1,62 @@
 class Dictionary {
-
   constructor() {
     this.data = {};
   }
-  add(key, value) {
+
+  //set(key, value): add a new element to the dictionary. If the key already exists, the existing value will be overwritten with the new one.
+  set(key, value) {
     this.data[key] = value;
   }
+
+  //get(key): return the value associated with the passed key.
   get(key) {
     return this.data[key];
   }
+
+  //hasKey(key): return true if the key exists in the dictionary
+  hasKey(key) {
+    return this.data[key] != null;
+  }
+
+  /**
+   * remove the value from the dictionary
+   * @param {string} key
+   */
+  remove(key) {
+    if (this.hasKey(key)) {
+      delete this.data[key];
+      return true;
+    }
+    return false;
+  }
+
+  //keyValues : return an array containing all the pairs.
+  keyValues() {
+    return Object.values(this.data);
+  }
+
+  //keys: return all the keys in the dictionary.
+  keys() {
+    let keysArray = [];
+    for (const key in this.data) {
+      keysArray.push(key);
+    }
+    return keysArray;
+  }
+
+  //values: return all the values in the dictionary.
+  values() {
+    let valuesArray = [];
+    for (const key in this.data) {
+      console.log(key);
+      // 'key1'
+      // 'key2'
+      valuesArray.push(this.data[key]);
+    }
+    return valuesArray;
+  }
 }
 const dictionary = new Dictionary();
-dictionary.add("Undefined Integers", { name: "CaptainCorey" });
-
-//const undefinedIntegers = dictionary.get("undefined integers");
+dictionary.set("Undefined Integers", { name: "CaptainCorey" });
 
 module.exports = Dictionary;
--- dictionary/Dictionary.test.js
+++ dictionary/Dictionary.test.js
@@ -1,11 +1,70 @@
 const { TestScheduler } = require("jest");
 const Dictionary = require("./Dictionary");
 
-test("First Test", () => {
+test("Set Test", () => {
   //GIVEN
   const dictionary = new Dictionary();
   //WHEN
-  dictionary.add("key", "value");
+  dictionary.set("key", "value");
   //THEN
   expect(dictionary.get("key")).toEqual("value");
 });
+
+test("Get Test", () => {
+  //GIVEN
+  const dictionary = new Dictionary();
+  dictionary.set("key", "value");
+  //WHEN
+  const value = dictionary.get("key");
+  //THEN
+  expect(value).toEqual("value");
+});
+
+test("HasKey Test", () => {
+  //GIVEN
+  const dictionary = new Dictionary();
+  dictionary.set("key", "value");
+  //WHEN
+  const value = dictionary.hasKey("key");
+  //THEN
+  expect(value).toEqual(true);
+});
+
+test("Remove Test", () => {
+  //GIVEN
+  const dictionary = new Dictionary();
+  dictionary.set("key", "value");
+  //WHEN
+  dictionary.remove("key");
+  //THEN
+  expect(dictionary.hasKey("key")).toEqual(false);
+});
+test("KeyValues Test", () => {
+    //GIVEN
+    const dictionary = new Dictionary();
+    dictionary.set("key", "value");
+    //WHEN
+    dictionary.remove("key");
+    //THEN
+    expect(dictionary.hasKey("key")).toEqual(false);
+  });
+  test("Keys Test", () => {
+    //GIVEN
+    const dictionary = new Dictionary();
+    dictionary.set("key1", "value1");
+    dictionary.set("key2", "value2");
+    //WHEN
+    const keys = dictionary.keys();
+    //THEN
+    expect(keys).toEqual(["key1","key2"]);
+  });
+  test("Values Test", () => {
+    //GIVEN
+    const dictionary = new Dictionary();
+    dictionary.set("key1", "value1");
+    dictionary.set("key2", "value2");
+    //WHEN
+    const values = dictionary.values();
+    //THEN
+    expect(values).toEqual(["value1","value2"]);
+  });
